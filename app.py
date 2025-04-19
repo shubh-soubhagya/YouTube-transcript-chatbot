fddf76cc2c14b9c22d23ddc317039e7d8c2d8add
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-04-19 12:47:10 +0530

    -

--- app.py
+++ app.py
@@ -131,5 +131,11 @@ def ask_question():
 def serve_static(path):
     return send_from_directory('static', path)
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT env from Render
+    app.run(host='0.0.0.0', port=port, debug=True)
+
